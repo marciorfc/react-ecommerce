f46ecf354debcc47e3a7241cdd790d9e4ac6ce52
marcio.carneiro@serpro.gov.br <mroberto@gmail.com>
2020-12-31 08:15:41 -0300

    added addItem and removeItem functionality to ccheckout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => isTheSameId(cartItem, cartItemToRemove));
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => !isTheSameId(cartItem, cartItemToRemove));
+    }
+
+    return cartItems.map(cartItem => 
+        isTheSameId(cartItem, cartItemToRemove) ? 
+        {
+            ...cartItem,
+            quantity: cartItem.quantity - 1
+        } : cartItem
+    );
+}
